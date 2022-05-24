0ff3a3a6a74c881c6d306019088e2e6da26ce31f
ducnd <ducnd@vmodev.com>
2022-05-24 16:54:05 +0700

    update main and add test.json

--- main.py
+++ main.py
@@ -1,16 +1,6 @@
-# This is a sample Python script.
+from user_profile import UserProfile
 
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    user_profile = UserProfile('ducnd', '123456')
+    print(f"username: {user_profile.username}, password: {user_profile.password}")
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
